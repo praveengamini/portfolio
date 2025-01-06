ca5b940c1dde1b47f5555e1e084cce219cd28f47
praveen <praveengamini009@gmail.com>
2025-01-07 00:52:52 +0530

    all are commiting

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'), // Add this line
     },
-  },
-  build: {
-    // Remove the external config for react-tilt
   },
 });
